a873e5b07df1fd1e380258396b70dec88e7b3413
abdullahyusuf01 <abdullah.yusuf.alfatih@gmail.com>
2021-07-08 13:23:32 +0700

    Poject UAS Ecomm

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$host = "sql102.epizy.com";
+$username = "epiz_29080924";
+$password = "DYJEi9iiSUh";
+$database_name = "epiz_29080924_ci_shop";
+
+$koneksi = mysqli_connect($host, $username, $password, $database_name) or die ("Koneksi Gagal");
+
+?>
